a1742f235626267d595ab4148b8357d790592ffb
Mandy Wong <mandy77@students.cs.ubc.ca>
2020-11-16 19:39:45 -0800

    added inspector class

--- src/main/java/data/Inspector.java
+++ src/main/java/data/Inspector.java
@@ -1,4 +1,13 @@
 package data;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
 public class Inspector {
+    public static void main(String[] args) {
+        // Parse the code you want to inspect:
+        CompilationUnit cu = StaticJavaParser.parse("class X { int x; }");
+        // Now comes the inspection code:
+        System.out.println(cu);
+    }
 }
