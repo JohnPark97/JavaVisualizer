fce44e4898ca3b7f94e30006d14c870b099bd71d
Mandy Wong <mandy77@students.cs.ubc.ca>
2020-11-22 03:45:07 -0800

    moved json creator into own class

--- src/main/java/data/Main.java
+++ src/main/java/data/Main.java
@@ -68,7 +68,8 @@ public class Main {
         }
         //Checking the dependencies
         checkDependencies(ListOfJavaClasses);
-        JSONObject jsonProject = createJSON(ListOfJavaClasses);
+        JSONConvertor convertor = new JSONConvertor();
+        JSONObject jsonProject = JSONConvertor.createJSON(ListOfJavaClasses);
 
 
         try (FileWriter file = new FileWriter("assets/project.json")) {
@@ -100,80 +101,6 @@ public class Main {
         }
     }
 
-    public static JSONObject createJSON(List<JavaClass> list) {
-        JSONObject jsonProject = new JSONObject();
-        List<JSONObject> classes = new ArrayList<JSONObject>();
-        List<JSONObject> links = new ArrayList<JSONObject>();
-        for (JavaClass jc : list) {
-            classes.add(JSONClass(jc));
-            links.add(JSONDependency(jc));
-        }
-        jsonProject.put("classes", classes);
-        jsonProject.put("links", links);
-
-        return jsonProject;
-    }
-
-    public static JSONObject JSONClass(JavaClass jc) {
-        JSONObject jsonClass = new JSONObject();
-        jsonClass.put("name", jc.getClassName());
-        jsonClass.put("line_count", jc.getLineCount());
-        jsonClass.put("IsEnumeration",jc.getEnum());
-        jsonClass.put("imports",jc.getImplements());
-        jsonClass.put("IsInterface", jc.getInterface());
-
-        List<JSONObject> methods = new ArrayList<JSONObject>();
-        for (JavaMethod m : jc.Methods) {
-            methods.add(JSONMethod(m));
-        }
-        jsonClass.put("methods", methods);
-
-        List<JSONObject> variables = new ArrayList<JSONObject>();
-        for (JavaVariable v : jc.GlobalVariables) {
-            variables.add(JSONVariable(v));
-        }
-        jsonClass.put("variables", variables);
-
-        return jsonClass;
-    }
-
-    public static JSONObject JSONVariable(JavaVariable jv) {
-        JSONObject jsonVariable = new JSONObject();
-        jsonVariable.put("name", jv.getName());
-        jsonVariable.put("type",jv.getType());
-        jsonVariable.put("modifiers", jv.getModifiers());
-        return jsonVariable;
-    }
-
-    public static JSONObject JSONDependency(JavaClass jc) {
-        JSONObject jsonDep = new JSONObject();
-        jsonDep.put("Class",jc.getClassName());
-        jsonDep.put("Implementations", jc.getImplements());
-        jsonDep.put("Extensions", jc.getExtensions());
-        jsonDep.put("Dependencies", jc.getDependencies());
-        return jsonDep;
-    }
-
-    public static JSONObject JSONMethod(JavaMethod jm) {
-        JSONObject jsonMethod = new JSONObject();
-        jsonMethod.put("name", jm.getName());
-        jsonMethod.put("returnType", jm.getReturnType());
-        jsonMethod.put("IsConstructor", jm.getConstructor());
-        jsonMethod.put("modifiers", jm.getModifiers());
-        List<JSONObject> parameters = new ArrayList<JSONObject>();
-        for (JavaParameter p : jm.getParameterList()) {
-            parameters.add(JSONParameter(p));
-        }
-        jsonMethod.put("parameters", parameters);
-        return jsonMethod;
-    }
-
-    public static JSONObject JSONParameter(JavaParameter jp) {
-        JSONObject jsonParameter = new JSONObject();
-        jsonParameter.put("name", jp.getName());
-        jsonParameter.put("type", jp.getType());
-        return jsonParameter;
-    }
 }
 
 
