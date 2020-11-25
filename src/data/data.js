c0cd73d1926928eb36968a9b2e0fcb62c2124c9d
johnpark <parkjong@student.ubc.ca>
2020-11-24 20:02:32 -0800

    add await

--- src/data/data.js
+++ src/data/data.js
@@ -48,7 +48,7 @@ async function inputHashMap(listOfFiles) {
       const nextListOfFiles = await httpRequest(url, {
         method: 'GET',
       });
-      inputHashMap(nextListOfFiles);
+      await inputHashMap(nextListOfFiles);
     } else if (element['type'] === 'file' && isCodeFile(element['name'])) {
       const stringElement = element['download_url'];
       returnedList.push(stringElement);
